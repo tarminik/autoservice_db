75598f86bdeec58a490a319e093dc41192bd2a62
tarasov mihail <tarminik@gmail.com>
2023-05-22 19:42:19 +0300

    primary key

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA IF NOT EXISTS auto;
 -- Создание таблицы "customer"
 CREATE TABLE IF NOT EXISTS auto.customer
 (
-    customer_id INTEGER PRIMARY KEY,
+    customer_id SERIAL PRIMARY KEY,
     customer_nm VARCHAR(50),
     phone_no    VARCHAR(50),
     tg_username VARCHAR(50) NOT NULL,
@@ -16,8 +16,8 @@ CREATE TABLE IF NOT EXISTS auto.customer
 -- Создание таблицы "booking"
 CREATE TABLE IF NOT EXISTS auto.booking
 (
-    booking_id   INTEGER PRIMARY KEY,
-    client_id    INTEGER,
+    booking_id   SERIAL PRIMARY KEY,
+    client_id    SERIAL,
     session_dttm TIME NOT NULL,
     CONSTRAINT fk_booking_customer
         FOREIGN KEY (client_id) REFERENCES auto.customer (customer_id)
@@ -26,10 +26,11 @@ CREATE TABLE IF NOT EXISTS auto.booking
 -- Создание таблицы "sale"
 CREATE TABLE IF NOT EXISTS auto.sale
 (
-    customer_id  INTEGER,
+    customer_id  SERIAL,
     sale_pct     INTEGER CHECK (sale_pct >= 0 AND sale_pct >= 100),
     total_sales  REAL CHECK (total_sales >= 0),
     history_dttm TIME NOT NULL,
+    CONSTRAINT pk_sale PRIMARY KEY (customer_id, history_dttm),
     CONSTRAINT fk_sale_customer
         FOREIGN KEY (customer_id) REFERENCES auto.customer (customer_id)
 );
@@ -37,8 +38,8 @@ CREATE TABLE IF NOT EXISTS auto.sale
 -- Создание таблицы "car"
 CREATE TABLE IF NOT EXISTS auto.car
 (
-    car_id      INTEGER PRIMARY KEY,
-    customer_id INTEGER,
+    car_id      SERIAL PRIMARY KEY,
+    customer_id SERIAL,
     maker       VARCHAR(50),
     model       VARCHAR(50),
     year        INTEGER,
@@ -50,8 +51,8 @@ CREATE TABLE IF NOT EXISTS auto.car
 -- Создание таблицы "order"
 CREATE TABLE IF NOT EXISTS auto.order
 (
-    order_id    INTEGER PRIMARY KEY,
-    car_id      INTEGER,
+    order_id    SERIAL PRIMARY KEY,
+    car_id      SERIAL,
     open_dt     DATE,
     close_dt    DATE,
     payment_amt REAL,
@@ -63,7 +64,7 @@ CREATE TABLE IF NOT EXISTS auto.order
 -- Создание таблицы "details"
 CREATE TABLE IF NOT EXISTS auto.details
 (
-    detail_id  INTEGER PRIMARY KEY,
+    detail_id  SERIAL PRIMARY KEY,
     maker      VARCHAR(50),
     detail_cnt INTEGER CHECK (detail_cnt >= 0),
     price      REAL CHECK (price >= 0),
@@ -73,8 +74,9 @@ CREATE TABLE IF NOT EXISTS auto.details
 -- Создание таблицы "details_in_order"
 CREATE TABLE IF NOT EXISTS auto.details_in_order
 (
-    order_id  INTEGER,
-    detail_id INTEGER,
+    order_id  SERIAL,
+    detail_id SERIAL,
+    CONSTRAINT pk_details_in_order PRIMARY KEY (order_id, detail_id),
     CONSTRAINT fk_details_in_order_order
         FOREIGN KEY (order_id) REFERENCES auto.order (order_id),
     CONSTRAINT fk_details_in_order_details
@@ -84,7 +86,7 @@ CREATE TABLE IF NOT EXISTS auto.details_in_order
 -- Создание таблицы "service"
 CREATE TABLE IF NOT EXISTS auto.service
 (
-    service_id INTEGER PRIMARY KEY,
+    service_id SERIAL PRIMARY KEY,
     service_nm VARCHAR(50),
     price      REAL CHECK (price >= 0)
 );
@@ -92,8 +94,9 @@ CREATE TABLE IF NOT EXISTS auto.service
 -- Создание таблицы "service_in_order"
 CREATE TABLE IF NOT EXISTS auto.service_in_order
 (
-    order_id   INTEGER,
-    service_id INTEGER,
+    order_id   SERIAL,
+    service_id SERIAL,
+    CONSTRAINT pk_service_in_order PRIMARY KEY (order_id, service_id),
     CONSTRAINT fk_service_in_order_order
         FOREIGN KEY (order_id) REFERENCES auto.order (order_id),
     CONSTRAINT fk_service_in_order_service
